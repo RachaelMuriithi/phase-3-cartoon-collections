1a94e1f8ae78ba66dee12a7559d8ab12a0845984
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-27 14:06:54 +0300

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+# dwarves_names = []
+def roll_call_dwarves (arr_dwarf)
   # Your code here
+  arr_dwarf.each.with_index(1) { |dwarf_name, index|
+    puts "#{index}. #{dwarf_name}"
+  }  
 end
 
-def summon_captain_planet# code an argument here
+# planeteer_calls = []
+def summon_captain_planet (planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |planeteer|
+    planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (arr_calls) # code an argument here
   # Your code here
+  arr_calls.any? {  |call|
+  call.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (arr_strings)  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr_strings.find { |string|
+  cheese_types.include?(string)
+  }
 end
